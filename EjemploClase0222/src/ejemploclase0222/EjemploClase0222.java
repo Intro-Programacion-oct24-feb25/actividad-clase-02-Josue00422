3abec53e85c58a8195bdd9e45e253830f7a2b67f
jhnadry naula <jjnaula3@utpl.edu.ec>
2024-10-17 10:46:37 -0500

    viva la vida

---
+++ EjemploClase0222/src/ejemploclase0222/EjemploClase0222.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemploclase0222;
+
+/**
+ *
+ * @author utpl
+ */
+public class EjemploClase0222 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Trabajando en NetBeans");
+        
+    }
+    
+}
